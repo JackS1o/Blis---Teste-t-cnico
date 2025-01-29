c6e3cc351e5cc00fd60d032092d95137a36805f6
Jackson <jviana116@gmail.com>
2025-01-29 16:13:37 -0300

    feat: init front

---
+++ frontend/src/app/app.routes.ts
@@ -0,0 +1,25 @@
+import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { LayoutComponent } from './components/layout/layout.component';
+import { RegisterComponent } from './components/register/register.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: '',
+    component: LayoutComponent,
+    children: []
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+  }
+];
